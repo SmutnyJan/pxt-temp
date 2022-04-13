48ce3a24a695c850de351ef0c2caa81208db7b51
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-13 21:42:10 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,42 @@
-input.onGesture(Gesture.TiltRight, function () {
-    drawing.moveInDirection(MoveDirection.Right)
-})
-input.onGesture(Gesture.LogoDown, function () {
-    drawing.moveInDirection(MoveDirection.Down)
-})
+let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
-    drawing.toogleCursor()
+    x += -1
+    if (x < 0) {
+        x = 4
+    }
+    drawing.moveAt(x, y)
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    drawing.moveInDirection(MoveDirection.Left)
+input.onPinPressed(TouchPin.P2, function () {
+    y += 1
+    if (y > 4) {
+        y = 0
+    }
+    drawing.moveAt(x, y)
 })
-input.onGesture(Gesture.LogoUp, function () {
-    drawing.moveInDirection(MoveDirection.Up)
+input.onButtonPressed(Button.AB, function () {
+    drawing.toogleCursor(x, y)
 })
 input.onButtonPressed(Button.B, function () {
+    x += 1
+    if (x > 4) {
+        x = 0
+    }
+    drawing.moveAt(x, y)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    drawing.redraw()
+})
+input.onGesture(Gesture.Shake, function () {
     drawing.clear()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    drawing.redraw()
+    y += -1
+    if (y < 0) {
+        y = 4
+    }
+    drawing.moveAt(x, y)
 })
 basic.forever(function () {
-    drawing.blinkCursor()
+    drawing.blinkCursor(x, y)
 })
